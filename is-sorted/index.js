a5eba61db9c7e64f7d26e49ef33d008e0c070a9f
chen <cty9388@hotmail.com>
2019-03-04 12:08:59 +0800

    update is-sorted

--- is-sorted/index.js
+++ is-sorted/index.js
@@ -2,16 +2,20 @@ function defaultComparator(num1, num2) {
     return num1 - num2;
 }
 
-module.exports = function checksort(array, comparator) {
+function checksort(array, comparator) {
     if (!Array.isArray(array)) {
         throw new TypeError('Unexpected Type :' + (typeof array));
     }
 
     comparator = comparator || defaultComparator;
 
-    for (let i = 1, length = array.length; i < length; i++) {
+    for (let i = 1, length = array.length; i < length; ++i) {
         if (comparator(array[i - 1], array[i]) > 0) {
             return false;
         }
     }
+
+    return true;
 }
+
+module.exports = checksort;
