bc27c6892c708dc28151dd4ff28be5ac065a339a
chen <cty9388@hotmail.com>
2019-03-05 20:47:42 +0800

    create array-slice module

---
+++ array-slice/index.js
@@ -0,0 +1,27 @@
+function slice(arr, start, end) {
+    var len = arr.length;
+    var range = [];
+
+    start = idx(len, start);
+    end = idx(len, end, len);
+
+    while (start < end) {
+        range.push(arr[start++]);
+    }
+    return range;
+
+}
+
+function idx(len, pos, end) {
+    if (pos === null) {
+        pos = end || 0;
+    } else if (pos < 0) {
+        pos = Math.max(pos + len, 0);
+    } else {
+        pos = Math.min(pos, len);
+    }
+
+    return pos;
+}
+
+module.exports = slice;
