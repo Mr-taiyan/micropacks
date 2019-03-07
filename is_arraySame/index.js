043ea54e5deabd1e3a33ffbac40cbc1a18e64e25
chen <cty9388@hotmail.com>
2019-03-07 09:52:35 +0800

    create is_arraySame module

---
+++ is_arraySame/index.js
@@ -0,0 +1,15 @@
+function judge(arr1, arr2) {
+    if (!Array.isArray(arr1) && !Array(arr2 && arr1.length !== arr2.length)) {
+        return false;
+    }
+
+    for (let [k, v] of arr1.entries()) {
+        if (arr2[k] !== v) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+module.exports = judge;
