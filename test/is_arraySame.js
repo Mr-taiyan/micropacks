6896c24308c5ec38d0b20fd18de58a5de03bf820
chen <cty9388@hotmail.com>
2019-03-07 10:06:37 +0800

    create test for is-arraySame.js

---
+++ test/is_arraySame.js
@@ -0,0 +1,16 @@
+require('mocha');
+const assert = require('assert');
+const isArraySame = require('../is_arraySame');
+
+describe('Array', function () {
+    it('should be same', function () {
+        assert(isArraySame([1,2,3], [1,2,3]), 'expect to be same');
+    });
+});
+
+describe('Array', function () {
+    it('should not be same', function () {
+        assert(!isArraySame([1,2,3], [2,3,4]), 'expect not to be same');
+        assert(!isArraySame([1,2,3], [2,1,3]), 'expect not to be same');
+    });
+});
