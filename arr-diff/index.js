e4149ad7c491599c230e7b0d292dae79c2c7ff81
chen <cty9388@hotmail.com>
2019-03-12 10:11:23 +0800

    create arr-diff module

---
+++ arr-diff/index.js
@@ -0,0 +1,18 @@
+function diff(arr1, arr2) {
+    obj1 = {};
+    obj2 = {};
+    for (let x of arr1) {
+        obj[x] = true;
+    }
+
+    for (let x of arr2) {
+        if (!obj1[x]) {
+            obj2[x] = true;
+        }
+    }
+
+    return obj2.keys();
+
+}
+
+module.exports = diff;
