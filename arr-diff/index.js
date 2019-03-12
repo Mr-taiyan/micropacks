e3e8c4f10f76a1f4517156c8a9bc776789cb8c6e
chen <cty9388@hotmail.com>
2019-03-12 10:49:42 +0800

    update arr-diff module

--- arr-diff/index.js
+++ arr-diff/index.js
@@ -22,4 +22,43 @@ function diff(arr1, arr2) {
 
 }
 
-module.exports = diff;
+function diffArray(one, two) {
+    if (!Array.isArray(two)) {
+        return one.slice();
+    }
+
+    var tlen = two.length;
+    var olen = one.length;
+    var idx = -1;
+    var arr = [];
+
+    while (++idx < olen) {
+        var ele = one[idx];
+
+        var hasEle = false;
+        for (var i = 0; i < tlen; i++) {
+            var val = two[i];
+
+            if (ele === val) {
+                hasEle = true;
+                break;
+            }
+        }
+
+        if (hasEle === false) {
+            arr.push(ele);
+        }
+    }
+
+    return arr;
+}
+
+function arrDiff(arr/*, array*/) {
+    var len = arguments.length;
+    var idx = 0;
+    while (++idx < len) {
+        arr = diffArray(arr, arguments[idx]);
+    }
+    return arr;
+}
+
